a563c7d9ecc2b19b3485e52d96003c9a63bcf0d8
vnsavenko <savenko_vladimir@mail.ru>
2020-12-20 18:08:32 +0300

    L97. Tomcat project deploy

--- city-register/src/test/java/edu/javacourse/net/SimpleSocket.java
+++ city-register/src/test/java/edu/javacourse/net/SimpleSocket.java
@@ -11,12 +11,12 @@ public class SimpleSocket
 {
     @Test
     public void simpleSocket() throws IOException {
-        Socket socket = new Socket("java-course.ru",80);
+        Socket socket = new Socket("localhost",8080);
 
         InputStream inputStream = socket.getInputStream();
         OutputStream outputStream = socket.getOutputStream();
 
-        String command = "GET /sitemap.xml HTTP/1.1\r\nHost:java-course.ru\r\n\r\n";
+        String command = "GET / HTTP/1.1\r\nHost:java-course.ru\r\n\r\n";
         outputStream.write(command.getBytes());
         outputStream.flush();
 
